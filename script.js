3003cff239212367f211e6a21aeff2edad2775b0
Celeste Layne <layne.celeste@gmail.com>
2015-11-15 15:15:32 -0800

    initially show 10 articles

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 console.log("Sanity Check: JS is working!");
 
+// STRUCTURE
+	var button = document.querySelector('#load-more');
+
+// EVENTS
+
+	button.addEventListener('click', loadMoreArticles);
+
+// FUNCTIONS
+
+	function loadMoreArticles() {
+		
+	}
+
 
 		
 
